2ae080120e23550773236e2b8e7cf2af1d4b8542
FariaJaheen <fariajaheen@gmail.com>
2024-05-30 17:47:18 -0400

    Add files via upload

---
+++ fancy_credits.py
@@ -0,0 +1,14 @@
+# Fancy Credits
+# Demonstrates escape sequences
+print(
+        """
+        Fancy Credits
+        \ \ \ \ \ \ \
+             by
+        Michael Dawson
+        \ \ \ \ \ \ \
+        """
+    )
+print("Special thanks goes out to:")
+print("My hair stylist, Henry \'The Great,\' who never says \"can\'t.\"")
+input("\n\nPress the enter key to exit.")
