63cc7a294f8c6b675729feb34d609cfa4459233d
Faria Jaheen <fariajaheen@gmail.com>
2024-06-05 19:23:05 -0400

    Add files via upload

--- fancy_credits.py
+++ fancy_credits.py
@@ -12,3 +12,25 @@ print(
 print("Special thanks goes out to:")
 print("My hair stylist, Henry \'The Great,\' who never says \"can\'t.\"")
 input("\n\nPress the enter key to exit.")
+
+
+# Moving Forward a Tab Stop
+print("\t Fancy Credits")
+print("Fancy Credits")
+
+
+# Printing a Backslash: 2 backslashes are used to output 1 backslash
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\\ \\ \\ \\ \\ \\ \\")
+print("\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t \\ \\ \\ \\ \\ \\ \\")
+
+
+# Inserting the Quote
+print("My hairstylist, Henry \'the Great,\' who never says \"can\'t.\"")
+
+
+# Sounding the System Bell
+print("\a")
+
